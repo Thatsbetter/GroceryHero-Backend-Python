54b85f7f3f9f08ae7b4b20674325a01887d859af
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2023-12-22 02:27:49 +0100

    feature/refactor: Remove old initial scripts

--- migrations/shopping_item/initial.sql
+++
@@ -1,8 +0,0 @@
-create table shopping_item (
-    id serial primary key,
-    product varchar(128) not null,
-    count integer not null,
-    shopper int,
-    status varchar(32),
-    constraint fk_shopper foreign key (shopper) references registered_user(id)
-)
--- migrations/user_table/initial.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE registered_user (
-	id serial primary key,
-	name text not null,
-	age integer not null,
-	email text not null,
-	password text not null,
-	location text not null
-)
