22c2096a867e5911ee61b72fb60185634be3e39a
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2023-12-22 02:26:27 +0100

    feature/refactor: Add user entity

--- user/user_model.py
+++ user/user_model.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-from databasecredential import Credential
+from backend.config import Credential
 
 Base = declarative_base()
 conn_string = Credential().get_conn_uri()
@@ -10,12 +10,18 @@ db = create_engine(conn_string)
 
 
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
-    age = Column(Integer, nullable=False)
-    email = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
-    location = Column(String, nullable=False)
+    location = Column(String)
+    age = Column(Integer)
+    rating = Column(Float)
 
-Base.metadata.create_all(db)
+    # One-to-Many relationship with carts created by the user
+    created_carts = relationship('Cart', backref='creator', foreign_keys='Cart.user_id')
+
+    # One-to-Many relationship with carts delivered by the user
+    delivered_carts = relationship('Cart', backref='deliverer', foreign_keys='Cart.deliverer_id')
