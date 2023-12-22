9cf6b3d9d66b414438018c3d1a5aeb85a7fcdb96
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2023-12-22 02:25:29 +0100

    feature/refactor: Add database class

--- database.py
+++ database.py
@@ -1,8 +1,15 @@
+
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 from config import Credential
+class Database:
+    def __init__(self):
+        database_url = Credential().get_conn_uri()
+        self.engine = create_engine(database_url, pool_size=10, max_overflow=20)
+        self.Session = sessionmaker(bind=self.engine)
 
-DATABASE_URL = Credential().get_conn_uri()
+    def create_all_tables(self):
+        Base.metadata.create_all(bind=self.engine)
 
-engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=20)
-Session = sessionmaker(bind=engine)
+    def get_session(self):
+        return self.Session()
