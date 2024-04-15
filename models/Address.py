cc7421a1ebfa6f72e46f5c806caec32b8c556ec4
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2024-04-15 18:11:23 +0200

    #6: Add Address Table

---
+++ models/Address.py
@@ -0,0 +1,21 @@
+from databasecredential import Credential
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+conn_string = Credential().get_conn_uri()
+db = create_engine(conn_string)
+
+
+class ShoppingItem(Base):
+        __tablename__ = "address"
+        id = Column(Integer, primary_key=True)
+        country = Column(String, nullable=False)
+        city = Column(String, nullable=False)
+        street = Column(String, nullable=False)
+        house_number = Column(Integer, nullable=True)
+        postal_code = Column(Integer, nullable=False)
+        additional_comment = Column(String, nullable=True)
+
+
+Base.metadata.create_all(db)
