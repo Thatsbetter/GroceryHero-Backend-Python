1b464f0e253d0602fd1f926c09b0dd4cee8bd180
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2024-04-16 11:29:11 +0200

    #6: Refactor ShoppingItem

--- models/ShoppingItem.py
+++ models/ShoppingItem.py
@@ -1,7 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
 from databasecredential import Credential
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 conn_string = Credential().get_conn_uri()
@@ -11,11 +10,7 @@ db = create_engine(conn_string)
 class ShoppingItem(Base):
         __tablename__ = "shopping_item"
         id = Column(Integer, primary_key=True)
-        product = Column(String)
-        count = Column(Integer)
-        shopper = Column(Integer) #, ForeignKey(RegisteredUser.id))
-        status = Column(String)
-
-        # registered_user = relationship('RegisteredUser', foreign_keys='ShoppingItem.id')
+        product = Column(String, ForeignKey('product.product_id'), nullable=False)
+        quantity = Column(Integer, nullable=False)
 
 Base.metadata.create_all(db)
