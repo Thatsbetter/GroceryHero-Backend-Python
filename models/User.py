5921e30b6407505a394d5faa34aed862a3c019b3
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2024-04-16 12:11:27 +0200

    #6: Add unique to User Schema

--- models/User.py
+++ models/User.py
@@ -13,6 +13,6 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     birthday = Column(DateTime, nullable=False)
-    email = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
 
