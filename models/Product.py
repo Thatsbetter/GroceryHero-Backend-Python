4a8aa538518e61a2bd452e478b734c80af0440f3
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2024-04-16 11:28:32 +0200

    #6: Add Product Table Schema

---
+++ models/Product.py
@@ -0,0 +1,21 @@
+from databasecredential import Credential
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+conn_string = Credential().get_conn_uri()
+db = create_engine(conn_string)
+
+
+class Product(Base):
+    __tablename__ = "product"
+    product_id = Column(Integer, primary_key=True)
+    supermarket = Column(String, nullable=True)
+    name = Column(String, nullable=False)
+    categories = Column(String, nullable=True)
+    price = Column(String, nullable=True)
+    packing = Column(String, nullable=True)
+    image = Column(String, nullable=False)
+
+
+Base.metadata.create_all(db)
