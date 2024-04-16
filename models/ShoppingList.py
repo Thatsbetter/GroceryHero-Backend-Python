ce57f2ba9fb38b73d7b26c422f6770f4aedda30c
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2024-04-16 11:29:39 +0200

    #6: Refactor ShoppingList

--- models/ShoppingList.py
+++ models/ShoppingList.py
@@ -1,7 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, String, BOOLEAN
-from sqlalchemy.ext.declarative import declarative_base
-
 from databasecredential import Credential
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 conn_string = Credential().get_conn_uri()
@@ -11,12 +10,8 @@ db = create_engine(conn_string)
 class ShoppingList(Base):
     __tablename__ = "shopping_list"
     id = Column(Integer, primary_key=True)
-    item1 = Column(Integer)  # , ForeignKey(ShoppingItem.id))
-    item2 = Column(Integer)  # , ForeignKey(ShoppingItem.id))
-    item3 = Column(Integer)  # , ForeignKey(ShoppingItem.id))
-    status = Column(String)
-    created_by = Column(Integer)  # , ForeignKey(RegisteredUser.id))
-    allow_multiple_shoppers = Column(BOOLEAN)
-
+    status = Column(String, nullable=False)
+    created_by = Column(Integer, ForeignKey('user.id'))
+    delivery_address = Column(Integer, ForeignKey('address.id'))
 
 Base.metadata.create_all(db)
